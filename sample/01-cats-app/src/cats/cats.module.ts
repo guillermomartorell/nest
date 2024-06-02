34ec454ed225eaa76087ddaf8af672a76fd2b03b
guillermomartorell <1244512+guillermomartorell@users.noreply.github.com>
2024-06-01 21:37:15 -0300

    import mongoose module

--- sample/01-cats-app/src/cats/cats.module.ts
+++ sample/01-cats-app/src/cats/cats.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
 })
 export class CatsModule {}
